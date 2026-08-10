7e3663ffd8ba1361431e4c6a4abf2058f7ac8069
BELLY JOE BUENO BASADRE <132354249+bJOE2001@users.noreply.github.com>
2026-08-10 12:43:48 +0800

    v 1.4.0

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -95,7 +95,7 @@
       </div>
     </q-card>
     <div class="login-footer text-center">
-      &copy; 2026 Tagum City Hall. All Rights Reserved. | v1.3.0 |
+      &copy; 2026 Tagum City Hall. All Rights Reserved. | v1.4.0 |
       <router-link to="/development-team" class="login-footer-link">Development Team</router-link>
     </div>
   </q-page>
